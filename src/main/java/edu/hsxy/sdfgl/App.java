00ee464b99d431536e82b0350e0eedc3fd57f1ab
wangxiang1 <wangxiang1@bonc.com.cn>
2016-12-05 14:37:44 +0800

    q

--- src/main/java/edu/hsxy/sdfgl/App.java
+++ src/main/java/edu/hsxy/sdfgl/App.java
@@ -1,7 +1,5 @@
 package edu.hsxy.sdfgl;
 
-import org.springframework.boot.SpringApplication;
-
 /**
  * Hello world!
  *
@@ -10,6 +8,5 @@ public class App{
     public static void main( String[] args ) {
         System.out.println( "Hello World!" );
         
-        SpringApplication.run(App.class, args);
     }
 }
