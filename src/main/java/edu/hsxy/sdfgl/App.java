e177246917d477f23108932cdf8e644a0a0c3b3a
wangxiang1 <wangxiang1@bonc.com.cn>
2016-12-05 14:28:32 +0800

    11

--- src/main/java/edu/hsxy/sdfgl/App.java
+++ src/main/java/edu/hsxy/sdfgl/App.java
@@ -9,6 +9,7 @@ import org.springframework.boot.SpringApplication;
 public class App{
     public static void main( String[] args ) {
         System.out.println( "Hello World!" );
+        
         SpringApplication.run(App.class, args);
     }
 }
