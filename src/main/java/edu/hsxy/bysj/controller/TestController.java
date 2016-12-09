c11bdb6552a909ce77ed671d05f96b28ee2406f5
wangxiang1 <wangxiang1@bonc.com.cn>
2016-12-09 16:08:52 +0800

    rename

--- src/main/java/edu/hsxy/bysj/controller/TestController.java
+++ src/main/java/edu/hsxy/bysj/controller/TestController.java
@@ -6,18 +6,16 @@
  * Copyright (C) 2016,rgl. All rights reserved.
  * 
  */
-package com.spring.springboot.controller;
+package edu.hsxy.bysj.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-/** 
- * JDK 1.8 <br/>
- * ClassName: TestController <br/> 
- * author rgl <br/>
- * date: 2016年11月21日 下午5:46:23 <br/> 
- * 
- * 功能描述:
+
+/**
+ * @author wangxiang
+ * 功能描述：测试类
+ * @data 2016年12月9日
  */
 @RestController
 public class TestController {
